e55108551b254433b9d5dd8ee10b0c5b9ff29635
victorlee <lijiquan@mail.ustc.edu.cn>
2018-05-19 14:32:30 +0800

    add machinefile option

--- lab01/MPI/pcrun.sh
+++ lab01/MPI/pcrun.sh
@@ -2,25 +2,25 @@
 echo " "
 mpicc PrimeCnt.c -o PrimeCnt
 echo "Thread Number :1 "
-mpirun -np 1 ./PrimeCnt 1000
-mpirun -np 1 ./PrimeCnt 10000
-mpirun -np 1 ./PrimeCnt 100000
-mpirun -np 1 ./PrimeCnt 500000
+mpirun -machinefile mf.txt -np 1 ./PrimeCnt 1000
+mpirun -machinefile mf.txt -np 1 ./PrimeCnt 10000
+mpirun -machinefile mf.txt -np 1 ./PrimeCnt 100000
+mpirun -machinefile mf.txt -np 1 ./PrimeCnt 500000
 echo " "
 echo "Thread Number :2 "
-mpirun -np 2 ./PrimeCnt 1000
-mpirun -np 2 ./PrimeCnt 10000
-mpirun -np 2 ./PrimeCnt 100000
-mpirun -np 2 ./PrimeCnt 500000
+mpirun -machinefile mf.txt -np 2 ./PrimeCnt 1000
+mpirun -machinefile mf.txt -np 2 ./PrimeCnt 10000
+mpirun -machinefile mf.txt -np 2 ./PrimeCnt 100000
+mpirun -machinefile mf.txt -np 2 ./PrimeCnt 500000
 echo " "
 echo "Thread Number :4 "
-mpirun -np 4 ./PrimeCnt 1000
-mpirun -np 4 ./PrimeCnt 10000
-mpirun -np 4 ./PrimeCnt 100000
-mpirun -np 4 ./PrimeCnt 500000
+mpirun -machinefile mf.txt -np 4 ./PrimeCnt 1000
+mpirun -machinefile mf.txt -np 4 ./PrimeCnt 10000
+mpirun -machinefile mf.txt -np 4 ./PrimeCnt 100000
+mpirun -machinefile mf.txt -np 4 ./PrimeCnt 500000
 echo " "
 echo "Thread Number :8 "
-mpirun -np 8 ./PrimeCnt 1000
-mpirun -np 8 ./PrimeCnt 10000
-mpirun -np 8 ./PrimeCnt 100000
-mpirun -np 8 ./PrimeCnt 500000
+mpirun -machinefile mf.txt -np 8 ./PrimeCnt 1000
+mpirun -machinefile mf.txt -np 8 ./PrimeCnt 10000
+mpirun -machinefile mf.txt -np 8 ./PrimeCnt 100000
+mpirun -machinefile mf.txt -np 8 ./PrimeCnt 500000
--- lab01/MPI/sprun.sh
+++ lab01/MPI/sprun.sh
@@ -2,25 +2,25 @@
 echo " "
 mpicc SolvePi.c -o SolvePi
 echo "Thread Number :1 "
-mpirun -np 1 ./SolvePi 1000
-mpirun -np 1 ./SolvePi 10000
-mpirun -np 1 ./SolvePi 50000
-mpirun -np 1 ./SolvePi 100000
+mpirun -machinefile mf.txt -np 1 ./SolvePi 1000
+mpirun -machinefile mf.txt -np 1 ./SolvePi 10000
+mpirun -machinefile mf.txt -np 1 ./SolvePi 50000
+mpirun -machinefile mf.txt -np 1 ./SolvePi 100000
 echo " "
 echo "Thread Number :2 "
-mpirun -np 2 ./SolvePi 1000
-mpirun -np 2 ./SolvePi 10000
-mpirun -np 2 ./SolvePi 50000
-mpirun -np 2 ./SolvePi 100000
+mpirun -machinefile mf.txt -np 2 ./SolvePi 1000
+mpirun -machinefile mf.txt -np 2 ./SolvePi 10000
+mpirun -machinefile mf.txt -np 2 ./SolvePi 50000
+mpirun -machinefile mf.txt -np 2 ./SolvePi 100000
 echo " "
 echo "Thread Number :4 "
-mpirun -np 4 ./SolvePi 1000
-mpirun -np 4 ./SolvePi 10000
-mpirun -np 4 ./SolvePi 50000
-mpirun -np 4 ./SolvePi 100000
+mpirun -machinefile mf.txt -np 4 ./SolvePi 1000
+mpirun -machinefile mf.txt -np 4 ./SolvePi 10000
+mpirun -machinefile mf.txt -np 4 ./SolvePi 50000
+mpirun -machinefile mf.txt -np 4 ./SolvePi 100000
 echo " "
 echo "Thread Number :8 "
-mpirun -np 8 ./SolvePi 1000
-mpirun -np 8 ./SolvePi 10000
-mpirun -np 8 ./SolvePi 50000
-mpirun -np 8 ./SolvePi 100000
+mpirun -machinefile mf.txt -np 8 ./SolvePi 1000
+mpirun -machinefile mf.txt -np 8 ./SolvePi 10000
+mpirun -machinefile mf.txt -np 8 ./SolvePi 50000
+mpirun -machinefile mf.txt -np 8 ./SolvePi 100000
